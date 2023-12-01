aca90fd4899ad4c8c003e149b8aa36f7394bad13
Tanki <mjrcoetzee+1@outlook.com>
2023-12-01 23:23:32 +0200

    add tsconfig actually use tsc to compile (makes the same file anyway lol)

--- dist/main.js
+++ dist/main.js
@@ -1,18 +1,19 @@
 const addon = {
-  name: "Template Addon",
-  description: "Template addon for YADMB.",
-  credits: "your-name",
-  version: "1.0.0",
-  type: "command",
-  sources: [
-    "https://github.com/tairasoul/yadmb-extension-template/tree/main"
-  ],
-  commands: {
-    name: "template-addon",
-    description: "template addon",
-    options: [],
-    callback: async (interaction) => await interaction.createMessage({content: "hello from template-addon!"})
-  }
-}
-
-export default addon
+    name: "Template Addon",
+    description: "Template addon for YADMB.",
+    credits: "your-name",
+    version: "1.0.0",
+    type: "command",
+    sources: [
+        "https://github.com/tairasoul/yadmb-extension-template/tree/main"
+    ],
+    commands: [
+        {
+            name: "template-addon",
+            description: "template addon",
+            options: [],
+            callback: async (interaction) => await interaction.createMessage({ content: "hello from template-addon!" })
+        }
+    ]
+};
+export default addon;
